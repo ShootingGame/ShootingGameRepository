db5777f476b5af1234f41e2bf66380a9f0a032ab
GoOshima <goshima5@gmail.com>
2014-12-21 18:14:44 +0900

    BGM追加
    BGMを追加

---
+++ ShootingGame/Assets/Scripts/Enemy/EnemyBoss.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBoss : Enemy
+{
+	//ボス移動追加分
+	public int count = 0; 
+	//ボス移動追加分
+	public float delta = 1;
+	
+	//ボス追加分(軌道部分)
+	void Update(){
+		Move (transform.right * delta);
+		count++;
+		if(count >= 20){
+			delta = delta * -1;
+			count = -40;
+		}
+	}
+
+	// 機体の移動
+	public void Move (Vector2 direction)
+	{
+		rigidbody2D.velocity = direction * spaceship.speed;
+	}
+
+	IEnumerator Start ()
+	{
+		this.setUp ();
+
+		// canShotがfalseの場合、ここでコルーチンを終了させる
+		if (spaceship.canShot == false) {
+			yield break;
+		}
+
+		while (true) {
+			// 子要素を全て取得する
+			for (int i = 0; i < transform.childCount; i++) {				
+				Transform shotPosition = transform.GetChild (i);
+				spaceship.EnemyShot(shotPosition);
+
+			}
+			// shotDelay秒待つ
+			yield return new WaitForSeconds (spaceship.shotDelay);
+		}
+
+	}
+
+	//ぶつかってくるのは自機の弾丸のみ想定
+	//自機がダメージを受ける処理はプレイヤー側に記述
+	void OnTriggerEnter2D (Collider2D c)
+	{
+		//共通処理
+		this.checkCollision (c);
+	}
+}
